4d1a3850ad74562121e5d849e88bc6ac494edf10
Hyunsu Kim <hyunsu.j.kim@gmail.com>
2020-06-04 10:56:18 -0500

    cleaned landing page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -6,49 +6,49 @@ import GoogleLogin from "../GoogleLogin/GoogleLogin";
 import "./LandingPage.css";
 
 class LandingPage extends Component {
-  state = {
-    heading: "Class Component",
-  };
+  // state = {
+  //   heading: "Class Component",
+  // };
 
-  onLogin = (event) => {
-    this.props.history.push("/login");
-  };
+  // onLogin = (event) => {
+  //   this.props.history.push("/login");
+  // };
 
-  onLogout = (event) => {
-    this.props.dispatch({ type: "LOGOUT" });
-  };
+  // onLogout = (event) => {
+  //   this.props.dispatch({ type: "LOGOUT" });
+  // };
 
   render() {
-    let loginOrLogout = (
-      <div className="grid-col grid-col_4">
-        <h3>Already a Member?</h3>
-        <button className="btn btn_sizeFull" onClick={this.onLogin}>
-          Login
-        </button>
-      </div>
-    );
+    // let loginOrLogout = (
+    //   <div className="grid-col grid-col_4">
+    //     <h3>Already a Member?</h3>
+    //     <button className="btn btn_sizeFull" onClick={this.onLogin}>
+    //       Login
+    //     </button>
+    //   </div>
+    // );
 
-    if (this.props.store.user.id != null) {
-      loginOrLogout = (
-        <div className="grid-col grid-col_4">
-          <h3>All done?</h3>
-          <button className="btn btn_sizeFull" onClick={this.onLogout}>
-            Logout
-          </button>
-        </div>
-      );
-    }
+    // if (this.props.store.user.id != null) {
+    //   loginOrLogout = (
+    //     <div className="grid-col grid-col_4">
+    //       <h3>All done?</h3>
+    //       <button className="btn btn_sizeFull" onClick={this.onLogout}>
+    //         Logout
+    //       </button>
+    //     </div>
+    //   );
+    // }
 
     return (
       <div className="container">
-        <h2>{this.state.heading}</h2>
-
+        {/* <h2>{this.state.heading}</h2> */}
+        {/* 
         <div className="grid">
-          <div className="grid-col grid-col_8">
-            <GoogleLogin />
-          </div>
-          {loginOrLogout}
-        </div>
+          <div className="grid-col grid-col_8"> */}
+        <GoogleLogin />
+        {/* </div> */}
+        {/* {loginOrLogout}
+        </div> */}
       </div>
     );
   }
