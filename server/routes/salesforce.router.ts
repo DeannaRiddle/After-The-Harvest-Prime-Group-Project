c6c747f69c3dc433f67bd967a9c6a6c323e22ee1
Deanna Riddle <lriddle.deanna@gmail.com>
2020-06-05 13:47:08 -0500

    salesforce route tested well

--- server/routes/salesforce.router.ts
+++ server/routes/salesforce.router.ts
@@ -12,7 +12,7 @@ const oauth2 = new jsforce.OAuth2({
   clientId: process.env.SALESFORCE_CLIENT_ID,
   clientSecret: process.env.SALESFORCE_SECRET_ID,
   //redirectUri : 'http://localhost:' + port +'/token'
-  redirectUri: "http://localhost:3000/list",
+  redirectUri: "http://localhost:3000/#/list",
 });
 /**
  * GET route template
@@ -21,32 +21,40 @@ router.get(
   "/auth/login",
   (req: Request, res: Response, next: express.NextFunction): void => {
     // Redirect to Salesforce login/authorization page
-    res.redirect(
-      oauth2.getAuthorizationUrl({ scope: "api id web refresh_token" })
-    );
+    console.log("is it working?");
+    try {
+      const redirectUrl = oauth2.getAuthorizationUrl({
+        scope: "api id web refresh_token",
+      });
+      console.log(redirectUrl);
+      res.redirect(redirectUrl);
+    } catch (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
   }
 );
 
-router.get(
-  "/list",
-  (req: Request, res: Response, next: express.NextFunction): void => {
-    const conn = new jsforce.Connection({ oauth2: oauth2 });
-    const code = req.query.code;
-    conn.authorize(code, function (err, userInfo) {
-      if (err) {
-        return console.error("This error is in the auth callback: " + err);
-      }
-      console.log("Access Token: " + conn.accessToken);
-      console.log("Instance URL: " + conn.instanceUrl);
-      console.log("refreshToken: " + conn.refreshToken);
-      console.log("User ID: " + userInfo.id);
-      console.log("Org ID: " + userInfo.organizationId);
-      req.session.accessToken = conn.accessToken;
-      req.session.instanceUrl = conn.instanceUrl;
-      req.session.refreshToken = conn.refreshToken;
-      var string = encodeURIComponent("true");
-    });
-  }
-);
+//router.get(
+//  "/list",
+//   (req: Request, res: Response, next: express.NextFunction): void => {
+//     const conn = new jsforce.Connection({ oauth2: oauth2 });
+//     const code = req.query.code;
+//     conn.authorize(code, function (err, userInfo) {
+//       if (err) {
+//         return console.error("This error is in the auth callback: " + err);
+//       }
+//       console.log("Access Token: " + conn.accessToken);
+//       console.log("Instance URL: " + conn.instanceUrl);
+//       console.log("refreshToken: " + conn.refreshToken);
+//       console.log("User ID: " + userInfo.id);
+//       console.log("Org ID: " + userInfo.organizationId);
+//       req.session.accessToken = conn.accessToken;
+//       req.session.instanceUrl = conn.instanceUrl;
+//       req.session.refreshToken = conn.refreshToken;
+//       var string = encodeURIComponent("true");
+//     });
+//   }
+// );
 
 export default router;
