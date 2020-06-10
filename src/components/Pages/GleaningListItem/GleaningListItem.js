3506f93ec722b67cd7fd860c344209e3bc30dc3c
Pamela Anderson <cyberryder2010@gmail.com>
2020-06-10 12:33:16 -0500

    gleaning list page color style update text align addedand objects placed in date order

--- src/components/Pages/GleaningListItem/GleaningListItem.js
+++ src/components/Pages/GleaningListItem/GleaningListItem.js
@@ -18,44 +18,56 @@ import {
 // create custom material styling
 const customStyles = (theme) =>
   createStyles({
-    imgMedia: {
-      height: "335px",
-      backgroundSizing: "cover",
+    orange: {
+      maxWidth: 345,
+      background: "#ed6622",
+      boxShadow: "1px 3px rgb(0,0,0,0.3)",
+    },
+    stuff: {
+      background: "#ffffff",
     },
   });
 
-const useStyles = makeStyles((theme) => ({
-  orange: {
-    maxWidth: 345,
-    background: "#ed6622",
-  },
-  stuff: {
-    background: "#eeeeee",
-  },
-}));
+class GleaningListItem extends Component {
+  //each item on click goes to the detail page
+  clickGleaningDetails = (event, id) => {
+    this.props.history.push(`/details/${id}`);
+  };
+  render() {
+    const { item, classes } = this.props;
 
-// const clickDetails = (clickGleaningDetails = (event, id) => {
-//   this.props.history.push(`/details/${id}`);
-// });
-//each item on click goes to the detail page
-function GleaningListItem() {
-  const { item } = this.props;
-  const classes = useStyles();
-  // const click = clickDetails();
-
-  return (
-    <Card className={classes.orange}>
-      <CardHeader title={item.farm} subheader={item.date} />
-      {/* <CardActionArea onClick={(event) => this.clickDetails(event)}> */}
-      <CardContent className={classes.stuff}>
-        <Typography variant="body2" color="textSecondary" component="p">
-          Field Supervisor: {item.field_supv}
-          Start Time: {item.start_time}
-        </Typography>
-      </CardContent>
-      {/* </CardActionArea> */}
-    </Card>
-  );
+    return (
+      <Card className={classes.orange}>
+        <CardHeader title={item.farm} subheader={item.date} />
+        <CardContent className={classes.stuff}>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Field Supervisor: {item.field_supv}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Start Time: {item.start_time}
+          </Typography>
+        </CardContent>
+        <CardActionArea onClick={(event) => this.clickGleaningDetails(event)}>
+          <Typography
+            variant="h5"
+            color="textSecondary"
+            component="p"
+            align="center"
+          >
+            {item.produce}
+          </Typography>
+          <Typography
+            variant="body1"
+            color="textSecondary"
+            component="p"
+            align="center"
+          >
+            click for details
+          </Typography>
+        </CardActionArea>
+      </Card>
+    );
+  }
 }
 
 const mapStoreToProps = (store) => ({ store });
