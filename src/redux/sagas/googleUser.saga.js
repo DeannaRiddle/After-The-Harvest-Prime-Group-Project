40f5abd5408f8d5ad9e0038cd26182c2988b54e4
Anne Culpepper <anne.culpepper@gmail.com>
2020-06-14 19:31:40 -0500

    sending google data over to server at /user route

--- src/redux/sagas/googleUser.saga.js
+++ src/redux/sagas/googleUser.saga.js
@@ -1,10 +1,9 @@
-// import axios from "axios";
+import axios from "axios";
 import { put, takeLatest } from "redux-saga/effects";
 
 function* setGoogleUserInfo(action) {
   try {
-    const userGoogleInfo = action.payload;
-    yield put({ type: "STORE_USER_GOOGLE_INFO", payload: userGoogleInfo });
+    yield axios.post("/api/google/user", action.payload);
   } catch (error) {
     console.log("Setting google user info failed", error);
   }
@@ -12,15 +11,27 @@ function* setGoogleUserInfo(action) {
 
 function* resetGoogleUserInfo(action) {
   try {
-    yield put({ type: "EMPTY_USER_GOOGLE_INFO" });
+    yield axios.post("/api/google/user", {});
   } catch (error) {
     console.log("Resetting google user info failed", error);
+  }
+}
+
+// yield here to the Google route (axios.post the userGoogleInfo to the server here)
+//create new function for CREATE_GOOGLE_DOCS
+
+function* createGoogleDocs(action) {
+  try {
+    yield axios.post("/api/google/docs", action.payload);
+  } catch (error) {
+    console.log("Creating google docs failed", error);
   }
 }
 
 function* googleUserSaga() {
   yield takeLatest("SET_USER_GOOGLE_INFO", setGoogleUserInfo);
   yield takeLatest("RESET_USER_GOOGLE_INFO", resetGoogleUserInfo);
+  yield takeLatest("CREATE_GOOGLE_DOCS", createGoogleDocs);
 }
 
 export default googleUserSaga;
