bda8375926dd66919e980fa8fd68956e7f6d075a
Anne Culpepper <anne.culpepper@gmail.com>
2020-06-18 12:38:27 -0500

    unmuted code that dispatches to server to create document in Google Docs on click of Export to Google Docs

--- src/components/GoogleDocs/GoogleDocsBtn.js
+++ src/components/GoogleDocs/GoogleDocsBtn.js
@@ -9,14 +9,14 @@ import mapStoreToProps from "../../redux/mapStoreToProps";
 
 class GoogleDocsBtn extends Component {
   createGoogleDoc = () => {
-    // this.props.dispatch({
-    //   type: "CREATE_GOOGLE_DOCS",
-    // });
     this.props.dispatch({
-      type: "CREATE_DOC_LINK",
-      payload:
-        "https://docs.google.com/document/d/13oJdNR__GQ74s3fAOTYAuFenyhl0TYknhb6IGIgI-94/edit?usp=sharing",
+      type: "CREATE_GOOGLE_DOCS",
     });
+    // this.props.dispatch({
+    //   type: "CREATE_DOC_LINK",
+    //   payload:
+    //     "https://docs.google.com/document/d/13oJdNR__GQ74s3fAOTYAuFenyhl0TYknhb6IGIgI-94/edit?usp=sharing",
+    // });
     this.props.history.push("/gleaning");
   };
 
